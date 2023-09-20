addac8405d030b429fb7b6df43d21ab9280e9671
fajrialfauzi <fajrial39@gmail.com>
2023-09-20 14:05:11 +0700

    membuat controller dan login user

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     
-
+    public function authenticate(Request $request)
+    {
+        $request-> validate([
+            'username' => 'required|email',
+            'password' => 'required',
+        ]);
+    }
 }
