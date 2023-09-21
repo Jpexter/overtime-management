66bf6055dd18badcc70869909f1dd1541787c185
AkbarZzzz <akbaralbarokah0@gmail.com>
2023-09-21 10:24:11 +0700

    menambah master data

--- login.php
+++ login.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page bg-dark">
 <div class="login-box">
@@ -97,15 +97,15 @@
   </script>
   
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- jquery-validation -->
-<script src="../../plugins/jquery-validation/jquery.validate.min.js"></script>
-<script src="../../plugins/jquery-validation/additional-methods.min.js"></script>
+<script src="plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="plugins/jquery-validation/additional-methods.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="dist/js/demo.js"></script>
 </body>
 </html>
---
+++ masterproject.php
@@ -0,0 +1,121 @@
+<?php 
+require"layout/header.php" ;
+require"layout/sidebar.php" ;
+?>
+
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Master Project</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <!-- /.row -->
+    <div class="row  d-flex align-items-center justify-content-center">
+        <div class="col-11">
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-sm bg-success" style="width: 70px;">
+                    <a href="addproject.php">+ Add</a>
+                </button>
+            </div>
+          <div class="card">
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+              <table class="table table-hover text-nowrap">
+                <thead class="bg-white">
+                  <tr>
+                    <th>No</th>
+                    <th>Project Name</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Rumah Sakit Jaya Abadi</td>
+                    <td>
+                      <button class="btn btn-xs btn-success">Active</button>
+                    </td>
+                    <td>
+                        <button class="btn">
+                            <a href="editproject.php">
+                                <i class="fas fa-edit text-warning"></i> <!-- Ikon Edit -->
+                            </a>  
+                        </button>
+                        <button class="btn"><i class="fas fa-trash text-danger"></i> <!-- Ikon Hapus --></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Go Bird</td>
+                    <td>
+                      <button class="btn btn-xs btn-success">Active</button>
+                    </td>
+                    <td>
+                        <button class="btn">
+                            <a href="editproject.php">
+                                <i class="fas fa-edit text-warning"></i> <!-- Ikon Edit -->
+                            </a>
+                        </button>
+                        <button class="btn"><i class="fas fa-trash text-danger"></i> <!-- Ikon Hapus --></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Kominfo</td>
+                    <td>
+                      <button class="btn btn-xs btn-success">Active</button>
+                    </td>
+                    <td>
+                        <button class="btn">
+                            <a href="editproject.php">
+                                <i class="fas fa-edit text-warning"></i> <!-- Ikon Edit -->
+                            </a>
+                        </button>
+                        <button class="btn"><i class="fas fa-trash text-danger"></i> <!-- Ikon Hapus --></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>BUMN</td>
+                    <td>
+                      <button class="btn btn-xs btn-success">Active</button>
+                    </td>
+                    <td>
+                        <button class="btn">
+                            <a href="editproject.php">
+                                <i class="fas fa-edit text-warning"></i> <!-- Ikon Edit -->
+                            </a>
+                        </button>
+                        <button class="btn"><i class="fas fa-trash text-danger"></i> <!-- Ikon Hapus --></button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+      <!-- /.row -->
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+<?php require"layout/footer.php" ; ?>
